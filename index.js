04f073707070b306fa43b3ec822117aba46a60fc
SHAKIB AHMMED <shakibahmmed1@gmail.com>
2026-03-04 17:22:53 +0600

    new branch created

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send("Hello from backend")
 })
 
-app.listen(port, () =>{
+
+
+app.get({
+
+})
+
+
+
+
+app.listen(port, () => {
     console.log(`Server running on ${port}`)
 })
